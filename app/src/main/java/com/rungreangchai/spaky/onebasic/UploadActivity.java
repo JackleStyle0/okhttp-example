9e573e54f8a94e32eb358a820d761e85282d4fb6
yaofunban <wucharat.th@gmail.com>
2016-09-08 11:22:17 +0700

    layout for upload product

--- app/src/main/java/com/rungreangchai/spaky/onebasic/UploadActivity.java
+++ app/src/main/java/com/rungreangchai/spaky/onebasic/UploadActivity.java
@@ -2,12 +2,23 @@ package com.rungreangchai.spaky.onebasic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class UploadActivity extends AppCompatActivity {
+    EditText edName, edNumber, edPrice;
+    ImageButton imgProduct;
+    Button btnUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+    }
+
+
+    public void bindWidget() {
+
     }
 }
